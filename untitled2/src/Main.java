507691fabe5714ae49a3d56b6f1908246873b503
Кирил Георгиев Димитров <KGDimitrov19@codingburgas.bg>
2023-03-01 09:12:43 +0200

    Deserealization

--- untitled2/src/Main.java
+++ untitled2/src/Main.java
@@ -1,18 +1,20 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         List<Integer> ints = new ArrayList<>();
-
-        String path="C:\\Users\\KGDimitrov19\\Documents>\\txt.txt";
+        ints.add(10);
+        ints.add(12);
+        ints.add(13);
+        String path = "C:\\Users\\KGDimitrov19\\Documents>\\txt.txt";
         FileOutputStream fileOutputStream = new FileOutputStream(path);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(ints);
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path));
+        List<Integer> deseriallizedInts = (List<Integer>) objectInputStream.readObject();
+        for (int i = 0; i < deseriallizedInts.size(); i++) {
+            System.out.println(deseriallizedInts.get(i));
+        }
     }
-
 }
 
