fdab453a3893b19a23bf3dfa97df99d4ee6e2974
KGDimitrov19 <KGDimitrov19@codingburgas.bg>
2023-04-19 08:35:05 +0300

    Box task for strings

--- untitled2/src/Main.java
+++ untitled2/src/Main.java
@@ -6,12 +6,15 @@ public class Main {
         ints.add(10);
         ints.add(12);
         ints.add(13);
+
         String path = "C:\\Users\\KGDimitrov19\\Documents>\\txt.txt";
         FileOutputStream fileOutputStream = new FileOutputStream(path);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(ints);
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path));
+
         List<Integer> deseriallizedInts = (List<Integer>) objectInputStream.readObject();
+
         for (int i = 0; i < deseriallizedInts.size(); i++) {
             System.out.println(deseriallizedInts.get(i));
         }
